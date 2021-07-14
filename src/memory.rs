41a2bd7ed38558599f4397308411fe710b074548
Raddari <harveywyatt@hotmail.com>
2021-07-14 20:44:02 +0930

    Add Memory struct

---
+++ src/memory.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+pub struct Memory {
+    callstack: Vec<u16>,
+}
+
+impl Default for Memory {
+    fn default() -> Self {
+        Memory::new()
+    }
+}
+
+impl Memory {
+    pub fn new() -> Self {
+        Memory {
+            callstack: Vec::new(),
+        }
+    }
+
+    pub fn callstack_empty(&self) -> bool {
+        self.callstack.is_empty()
+    }
+
+    pub fn callstack_peek(&self) -> Option<u16> {
+        self.callstack.last().copied()
+    }
+
+    pub fn callstack_push(&mut self, address: u16) {
+        self.callstack.push(address);
+    }
+
+    pub fn callstack_pop(&mut self) -> Option<u16> {
+        self.callstack.pop()
+    }
+}
