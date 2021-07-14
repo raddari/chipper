7e3a1c53329d195807f536d55ed83b3a4469ba4a
Raddari <harveywyatt@hotmail.com>
2021-07-14 16:16:17 +0930

    Implement pc incrementing test

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Cpu {
     pc: u16,
 }
@@ -5,6 +6,10 @@ pub struct Cpu {
 impl Cpu {
     pub fn new() -> Cpu {
         Cpu { pc: 0x200 }
+    }
+
+    pub fn execute(&mut self, instruction: u16) {
+        self.pc += 2;
     }
 }
 
@@ -16,5 +21,12 @@ mod tests {
     fn pc_starts_at_0x200() {
         let cpu = Cpu::new();
         assert_eq!(0x200, cpu.pc);
+    }
+
+    #[test]
+    fn execute_normally_increments_pc() {
+        let mut cpu = Cpu::new();
+        cpu.execute(0x0);
+        assert_eq!(0x202, cpu.pc);
     }
 }
