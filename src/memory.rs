42600b600ae68a2c8d4af5cf98c6beca7ed86bf4
Raddari <harveywyatt@hotmail.com>
2021-07-15 21:25:15 +0930

    Add ram and functions to load/store

--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,6 @@
 #[derive(Debug)]
 pub struct Memory {
+    ram: [u8; 4096],
     callstack: Vec<u16>,
 }
 
@@ -12,8 +13,17 @@ impl Default for Memory {
 impl Memory {
     pub fn new() -> Self {
         Memory {
+            ram: [0; 4096],
             callstack: vec![0; 16],
         }
+    }
+
+    pub fn load(&self, offset: usize, size: usize) -> &[u8] {
+        &self.ram[offset..offset + size]
+    }
+
+    pub fn store(&mut self, offset: usize, bytes: &[u8]) {
+        self.ram[offset..offset + bytes.len()].copy_from_slice(bytes);
     }
 
     pub fn callstack_empty(&self) -> bool {
