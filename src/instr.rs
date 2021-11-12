aa4015f17b6805849b318a94dfa9beb75a754715
raddari <harveywyatt@hotmail.com>
2021-11-12 20:24:43 +1030

    Add instruction display

--- src/instr.rs
+++ src/instr.rs
@@ -54,3 +54,48 @@ pub enum Instr {
     StoreMem(V),
     LoadMem(V),
 }
+
+impl fmt::Display for Instr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Instr::*;
+
+        match self {
+            SysCall(nnn) => write!(f, "sys {:#05x}", nnn),
+            ClearScr => write!(f, "cls"),
+            Return => write!(f, "ret"),
+            Jump(nnn) => write!(f, "jal {:#05x}", nnn),
+            JumpOffset(nnn) => write!(f, "jalr {:#05x}({})", nnn, V(0x0)),
+            FnCall(nnn) => write!(f, "call {:#05x}", nnn),
+            SkipEqImm(x, kk) => write!(f, "sei {}, {}", x, kk),
+            SkipNeImm(x, kk) => write!(f, "snei {}, {}", x, kk),
+            SkipEqReg(x, y) => write!(f, "se {}, {}", x, y),
+            SkipNeReg(x, y) => write!(f, "sne {}, {}", x, y),
+            SkipEqKey(x) => write!(f, "skp {}", x),
+            SkipNeKey(x) => write!(f, "sknp {}", x),
+            LoadImm(x, kk) => write!(f, "ld {}, {}", x, kk),
+            LoadReg(x, y) => write!(f, "ld {}, {}", x, y),
+            AddImm(x, kk) => write!(f, "addi {}, {}", x, kk),
+            AddReg(x, y) => write!(f, "add {}, {}", x, y),
+            AddCarryReg(x, y) => write!(f, "addc {}, {}", x, y),
+            SubBorrowReg(x, y) => write!(f, "sub {}, {}", x, y),
+            SubnBorrowReg(x, y) => write!(f, "subn {}, {}, {}", x, y, x),
+            OrReg(x, y) => write!(f, "or {}, {}", x, y),
+            AndReg(x, y) => write!(f, "and {}, {}", x, y),
+            XorReg(x, y) => write!(f, "xor {}, {}", x, y),
+            ShrReg(x, y) => write!(f, "srl {}, {}", x, y),
+            ShlReg(x, y) => write!(f, "sll {}, {}", x, y),
+            LoadAddr(nnn) => write!(f, "ld I, {:#05x}(0)", nnn),
+            AddAddr(x) => write!(f, "ld I, {}({})", x, V(0x0)),
+            Rand(x, kk) => write!(f, "rnd {}, {:#05x}", x, kk),
+            Draw(x, y, n) => write!(f, "drw {}, {}, {}", x, y, n),
+            GetDelay(x) => write!(f, "ld {}, DT", x),
+            SetDelay(x) => write!(f, "ld DT, {}", x),
+            SetSound(x) => write!(f, "ld ST, {}", x),
+            WaitKey(x) => write!(f, "ld {}, $K", x),
+            LoadDigit(x) => write!(f, "ld I, {}", x),
+            StoreBcd(x) => write!(f, "bcd [I], {}", x),
+            StoreMem(x) => write!(f, "sd [I], {}", x),
+            LoadMem(x) => write!(f, "ld {}, [I]", x),
+        }
+    }
+}
